6b62a3047b1289ae5981708e1b7eafeebf0a96ea
Nick Torosian <goodvibrations32@protonmail.com>
2024-10-19 21:04:25 +0300

    basic setup that /might/ work

--- src/main.c
+++ src/main.c
@@ -1,13 +1,18 @@
 /* #include "include/stm32g0xx_hal_driver/Inc/stm32g0xx.h" */
 /* #include <stdio.h> */
+/* #include "system_stm32g0xx.c" */
 #include "../include/main.h"
 
-/* void SysInit(void) { */
-/* } */
+void SysInit(void) {
+  HAL_Init();
+  Ini
+}
 
-int main (){
-        /* printf("Hey arm1!!"); */
-        HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
-        int my_shit = 5;
-        return 0;
+int main() {
+  /* HAL_GPIO_Init(GPIOA, GPIO_PIN_13); */
+  int my_shit = 5;
+  while (my_shit < 10) {
+    my_shit = 10;
+  }
+  return my_shit;
 }
