06c687d05f273290f2e26e7dc11ab94c90678d53
Nick Torosian <goodvibrations32@protonmail.com>
2024-12-26 17:01:13 +0200

    rector:(macros) The size calculation should be asking for a wave. This is known behavior if you need to make the waveshape change on the fly. Try to make a nice wave creation function and put the data in the size calculation macro for modularity later.

--- src/wave.c
+++ src/wave.c
@@ -1,10 +1,10 @@
 #include "wave.h"
-#define DATA_SIZE         (uint32_t) sizeof(scaled_sin)/sizeof(scaled_sin[0])
+#define DATA_SIZE(wave)         (uint32_t) sizeof(wave)/sizeof(wave[0])
 
 wave_t create_wave(wave_t *empty, uint32_t freq, uint32_t amp, const uint16_t *data){
     empty->frequency = freq;
     empty->amplitude = amp;
     empty->form = data;
-    empty->size = DATA_SIZE;
+    empty->size = DATA_SIZE(scaled_sin);
     return *empty;
 }
