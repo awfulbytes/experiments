876675b9bf38da3d65d915c99aee024b1f85b6a5
Nick Torosian <goodvibrations32@protonmail.com>
2025-01-08 11:15:39 +0200

    minor things renaming for fast iteration and no arm lib colisions

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@ void main() {
   struct timer tim6_settings = {0};
   sys_clock_config();
   tim6_settings = *timx_set(&tim6_settings);
-  tim_init(&tim6_settings, 250, sin);
+  tim_init(&tim6_settings, 250, sine_wave);
 
   struct dac dac_settings = {0};
 
@@ -24,22 +24,22 @@ void main() {
   dac_act(&dac_settings);
   WaitForUserButtonPress(&ubButtonPress);
   while (1) {
-    if (ubButtonPress.flag == 'S') {
+    if (ubButtonPress.flag == 0x69) {
       switch (ubButtonPress.state) {
         case 0x0:
-          dma_change_wave(sin, 250, &tim6_settings);
-          LL_mDelay(400);
+          dma_change_wave(sine_wave, 250, &tim6_settings);
+          /* LL_mDelay(400); */
           LL_GPIO_TogglePin(LED4_GPIO_PORT, LED4_PIN);
           break;
         case 0x1:
           if (dma_change_wave(sawdn, 200, &tim6_settings) == SUCCESS){
             LL_GPIO_TogglePin(LED4_GPIO_PORT, LED4_PIN);
-            LL_mDelay(200);
+            /* LL_mDelay(200); */
           }
           break;
         case 0x2:
           if(dma_change_wave(sawup, 100, &tim6_settings) == SUCCESS){
-            LL_mDelay(500);
+            /* LL_mDelay(500); */
             LL_GPIO_TogglePin(LED4_GPIO_PORT, LED4_PIN);
           }
           break;
