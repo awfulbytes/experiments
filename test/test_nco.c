44e6ca0a72c135317c9b255b3cc63fd99a541a37
Nick Torosian <goodvibrations32@protonmail.com>
2025-02-17 14:48:21 +0200

    onto something

--- test/test_nco.c
+++ test/test_nco.c
@@ -12,9 +12,9 @@ uint16_t some2[256];
 uint16_t full[256];
 uint16_t sine_upd[256];
 
-uint32_t master_clock = 441000;
-uint32_t required_freq = 440;
-uint16_t acc_bits = sizeof(uint32_t) * 8;
+uint32_t master_clock = 44100;
+uint32_t required_freq = 444;
+uint16_t acc_bits = sizeof(phase_accum) * 8;
 
 void test_phase_increment(){
     // alter_wave_frequency(0xfff);
@@ -36,9 +36,10 @@ void test_ping_pong(){
 }
 
 int main(void){
-    uint64_t req = (phase_inc * master_clock) / (1<<acc_bits);
-    printf("%lx\n", req);
-    printf("%lu\n", sizeof(req)*8);
+    uint64_t new_incr = (required_freq * (1UL<<acc_bits)) / master_clock;
+    uint64_t new_req = ((new_incr * master_clock) >> acc_bits) + 1;
+    printf("%lx\n", new_incr);
+    printf("%lu\n", new_req);
     test_ping_pong();
     // test_phase_increment();
     alter_wave_frequency(0xfff);
