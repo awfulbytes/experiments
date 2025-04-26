89393fd24487627271b4a12e5a5c7d435f8ced61
Nick Torosian <goodvibrations32@protonmail.com>
2025-04-26 16:36:16 +0300

    make hack ready

--- src/stm32g0xx_it.c
+++ src/stm32g0xx_it.c
@@ -45,6 +45,19 @@ void DMA1_Channel2_3_IRQHandler(void){
         phase_done_update = false;
     }
 
+    // HACK:: this could eliminate the latency and sync the oscillators...
+    //        if that's the case we can have true sync and not first
+    //        second oscillators...
+    //        TODO:: the same for TC interrupt
+    // if (((DMA1->ISR & DMA_ISR_HTIF2) == DMA_ISR_HTIF2) &&
+    //     ((DMA1->ISR & DMA_ISR_HTIF3) == DMA_ISR_HTIF3) {
+    //    (DMA1->IFCR) = (DMA_IFCR_CHTIF2);
+    //    (DMA1->IFCR) = (DMA_IFCR_CHTIF3);
+    //    update_data_buff(l_osc.data_buff.ping_buff, dac_double_buff, 128);
+    //    update_data_buff(r_osc.data_buff.ping_buff, dac_double_buff2, 128);
+    // }
+    // HACK::
+
     if ((DMA1->ISR & DMA_ISR_HTIF2) == DMA_ISR_HTIF2) {
         (DMA1->IFCR) = (DMA_IFCR_CHTIF2);
         update_data_buff(r_osc.data_buff.ping_buff, dac_double_buff2, 128);
