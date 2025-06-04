4e478f6c9b3bb1b792fc3ffb498ba3192b563478
Nick Torosian <goodvibrations32@protonmail.com>
2025-06-04 15:25:34 +0300

    this is a relic of the past. we ain't so priviledged with ram and storage anymore. chould be healpful for the mts project.

--- src/wave.c
+++
@@ -1,9 +0,0 @@
-#include "wave.h"
-
-wave_t create_wave(wave_t *empty, uint32_t freq, uint32_t amp, const uint16_t *data){
-    empty->frequency = freq;
-    empty->amplitude = amp;
-    empty->form = data;
-    empty->size = DATA_SIZE(scaled_sin);
-    return *empty;
-}
