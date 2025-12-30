515ea2b3bbbcdd7987c0930c592d8d0d972cc84d
goodvibrations32 <goodvibrations32@protonmail.com>
2025-12-30 15:17:45 +0200

    fix: (128 spread) Explain what the fuck 128 is. Was about time!

--- src/stm32g0xx_it.c
+++ src/stm32g0xx_it.c
@@ -6,6 +6,7 @@
 #define lock_the_door(x)       __disable_irq();\
                                x;\
                                __enable_irq();
+#define buffer_size(buf)       ((sizeof buf) / (sizeof buf[0]))
 
 void HardFault_Handler(void){while(1);}
 void NMI_Handler(void){}
@@ -19,37 +20,52 @@ void TIM7_LPTIM2_IRQHandler(void){
         TIM7->SR &= ~(TIM_SR_UIF);
     }
 }
-uint32_t merged_dual_buffer[128];
 
-void merge_signals(void) {
-    for (int j=0; j<128; ++j){
-        merged_dual_buffer[j] = ((uint32_t)r_osc.data_buff.ping_buff[j] << 16U) \
-                                | l_osc.data_buff.ping_buff[j];
-    }
+uint32_t construct_dual_dac_reg(uint16_t data_ch_2, uint16_t data_ch_1) {
+    return ((uint32_t) data_ch_2 << 16U) | data_ch_1;
 }
 
+extern const uint16_t sine_wave[128];
+uint32_t merged_dual_buffer[buffer_size(sine_wave)];
+uint8_t const data_size = buffer_size(merged_dual_buffer);
 void DMA1_Channel2_3_IRQHandler(void){
 
     if (l_osc.phase.done_update && r_osc.phase.done_update) {
         l_osc.phase.inc = l_osc.phase.pending_update_inc;
         /* this was just for testing now i have to equalize adc pins */
         r_osc.phase.inc = l_osc.phase.pending_update_inc;
+        /* r_osc.phase.inc = r_osc.phase.pending_update_inc; */
 
-        generate_half_signal(wave_me_d, 128, &l_osc);
-        generate_half_signal(wave_me_d2, 128, &r_osc);
+        generate_half_signal(wave_me_d, data_size, &l_osc);
+        generate_half_signal(wave_me_d2, data_size, &r_osc);
         l_osc.phase.done_update = false;
         r_osc.phase.done_update = false;
     }
-    merge_signals();
-    if (((DMA1->ISR & DMA_ISR_HTIF2) == DMA_ISR_HTIF2) /* && ((DMA1->ISR & DMA_ISR_HTIF3) == DMA_ISR_HTIF3) */) {
-        update_data_buff(merged_dual_buffer, dac_double_buff2, 128);
+    for(uint8_t u=0; u < data_size; ++u){
+        merged_dual_buffer[u] =
+            construct_dual_dac_reg(r_osc.data_buff.ping_buff[u],
+                                   l_osc.data_buff.ping_buff[u]);
+    }
+    if (((DMA1->ISR & DMA_ISR_HTIF2) == DMA_ISR_HTIF2)) {
+        update_data_buff(merged_dual_buffer,
+                         dac_double_buff2,
+                         data_size);
         (DMA1->IFCR) = (DMA_IFCR_CHTIF2);
     }
 
-    if (((DMA1->ISR & DMA_ISR_TCIF2) == DMA_ISR_TCIF2) /* && ((DMA1->ISR & DMA_ISR_TCIF3) == DMA_ISR_TCIF3) */) {
-        update_data_buff(merged_dual_buffer, dac_double_buff2 + 128, 128);
+    if (((DMA1->ISR & DMA_ISR_TCIF2) == DMA_ISR_TCIF2)) {
+        update_data_buff(merged_dual_buffer,
+                         dac_double_buff2 + data_size,
+                         data_size);
         (DMA1->IFCR) = (DMA_IFCR_CTCIF2);
     }
+    /* todo it was fun as it lasted...
+     * we have an issue where when the distortion is enabled makes signals out
+     * of sync. No idea why!!! most of the times you should be able to
+     * invalidate the data and start over but here this does not work. nxt:: the
+     * phase.inc makes the phase not the data in the table those are one cycle
+     * always!!
+     */
 }
 
 void TIM2_IRQHandler(void) {
@@ -95,6 +111,7 @@ void EXTI4_15_IRQHandler(void) {
     }
     if ((EXTI->RPR1 & osc_0_pd_enc.A.it_settings.exti_line) == osc_0_pd_enc.A.it_settings.exti_line){
         osc_0_pd_enc.B.value[1] = read_gpio(&osc_0_pd_enc.B.pin);
+        /* r_osc.distortion.distortion_value = l_osc.distortion.distortion_value; */
         apply_modulations_callback(&osc_0_pd_enc, &l_osc);
         (EXTI->RPR1) = (osc_0_pd_enc.A.it_settings.exti_line);
     }
@@ -105,6 +122,7 @@ void EXTI4_15_IRQHandler(void) {
     }
     if ((EXTI->RPR1 & osc_1_pd_enc.A.it_settings.exti_line) == osc_1_pd_enc.A.it_settings.exti_line){
         osc_1_pd_enc.B.value[1] = read_gpio(&osc_1_pd_enc.B.pin);
+        /* r_osc.distortion.distortion_value = l_osc.distortion.distortion_value; */
         apply_modulations_callback(&osc_1_pd_enc, &r_osc);
         (EXTI->RPR1) = (osc_1_pd_enc.A.it_settings.exti_line);
     }
